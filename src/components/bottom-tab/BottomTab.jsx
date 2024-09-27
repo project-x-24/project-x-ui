8514af9fa06343da076fea5bd260eddac48d084d
shanidh-khan <shanidh@keyvalue.systems>
2024-09-28 04:10:36 +0530

    Bottom tab changes

--- src/components/bottom-tab/BottomTab.jsx
+++ src/components/bottom-tab/BottomTab.jsx
@@ -10,18 +10,18 @@ const BottomTab = ({ activeTab }) => {
     <div className="fixed bottom-0 w-full bg-white" style={tabShadow}>
       <div className="flex justify-between text-center">
         <div className="flex-1 flex flex-col justify-center items-center p-4 text-[12px]">
-          <HomeIcon className={`${activeTab === "home" ? "fill-[#EB5017]" : "fill-gray-500"}`} />
-          <span className={`mt-1 ${activeTab === "home" ? "text-[#EB5017]" : "text-gray-500"}`}>Home</span>
+          <HomeIcon className={`${activeTab === "home" ? "fill-[#EB5017]" : "fill-[#98A2B3]"}`} />
+          <span className={`mt-1 ${activeTab === "home" ? "text-[#EB5017]" : "text-[#98A2B3]"}`}>Home</span>
         </div>
 
         <div className="flex-1 flex flex-col justify-center items-center p-4 text-[12px]">
-          <PieIcon className={`${activeTab === "reports" ? "fill-[#EB5017]" : "fill-gray-500"}`} />
-          <span className={`mt-1 ${activeTab === "reports" ? "text-[#EB5017]" : "text-gray-500"}`}>Reports</span>
+          <PieIcon className={`${activeTab === "reports" ? "fill-[#EB5017]" : "fill-[#98A2B3]"}`} />
+          <span className={`mt-1 ${activeTab === "reports" ? "text-[#EB5017]" : "text-[#98A2B3]"}`}>Reports</span>
         </div>
 
         <div className="flex-1 flex flex-col justify-center items-center p-4 text-[12px]">
-          <MemoryIcon className={`${activeTab === "memory" ? "fill-[#EB5017]" : "fill-gray-500"}`} />
-          <span className={`mt-1 ${activeTab === "memory" ? "text-[#EB5017]" : "text-gray-500"}`}>Memory Lane</span>
+          <MemoryIcon className={`${activeTab === "memory" ? "fill-[#EB5017]" : "fill-[#98A2B3]"}`} />
+          <span className={`mt-1 ${activeTab === "memory" ? "text-[#EB5017]" : "text-[#98A2B3]"}`}>Memory Lane</span>
         </div>
       </div>
     </div>
