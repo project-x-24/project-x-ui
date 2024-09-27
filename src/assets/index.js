476c118980f80a7630cf6d08d2797268721f65a1
shanidh-khan <shanidh@keyvalue.systems>
2024-09-28 02:45:21 +0530

    Homepage design without tab

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,20 +1,37 @@
 export { ReactComponent as SampleIcon } from "../assets/icons/sample.svg";
 export { ReactComponent as RoundedTickIcon } from "../assets/icons/roundedTick.svg";
-export { ReactComponent as RoundedErrorIcon} from "../assets/icons/roundedError.svg";
-export { ReactComponent as LeftArrowIcon} from "../assets/icons/left-arrow.svg";
-export { ReactComponent as MenuIcon} from "../assets/icons/menu.svg";
-export { ReactComponent as SearchIcon} from "../assets/icons/search-icon.svg";
-export { ReactComponent as CameraIcon} from "../assets/icons/camera.svg";
-export { ReactComponent as AttachmentIcon} from "../assets/icons/attachment.svg";
-export { ReactComponent as SmileIcon} from "../assets/icons/smile.svg";
-export { ReactComponent as SendIcon} from "../assets/icons/send.svg";
-export { ReactComponent as CallIcon} from "../assets/icons/call.svg";
-export { ReactComponent as AboutIcon} from "../assets/icons/about.svg";
-export { ReactComponent as ChatIcon} from "../assets/icons/chat.svg";
-export { ReactComponent as EditRoundedIcon} from "../assets/icons/edit-rounded.svg";
-export { ReactComponent as PlayIcon} from "../assets/icons/play.svg";
-export { ReactComponent as VideoIcon} from "../assets/icons/video.svg";
-export  { ReactComponent as LeftRoundedIcon} from "../assets/icons/left-arrow-rounded.svg";
+export { ReactComponent as RoundedErrorIcon } from "../assets/icons/roundedError.svg";
+export { ReactComponent as LeftArrowIcon } from "../assets/icons/left-arrow.svg";
+export { ReactComponent as MenuIcon } from "../assets/icons/menu.svg";
+export { ReactComponent as SearchIcon } from "../assets/icons/search-icon.svg";
+export { ReactComponent as CameraIcon } from "../assets/icons/camera.svg";
+export { ReactComponent as AttachmentIcon } from "../assets/icons/attachment.svg";
+export { ReactComponent as SmileIcon } from "../assets/icons/smile.svg";
+export { ReactComponent as SendIcon } from "../assets/icons/send.svg";
+export { ReactComponent as CallIcon } from "../assets/icons/call.svg";
+export { ReactComponent as AboutIcon } from "../assets/icons/about.svg";
+export { ReactComponent as ChatIcon } from "../assets/icons/chat.svg";
+export { ReactComponent as EditRoundedIcon } from "../assets/icons/edit-rounded.svg";
+export { ReactComponent as PlayIcon } from "../assets/icons/play.svg";
+export { ReactComponent as VideoIcon } from "../assets/icons/video.svg";
+export { ReactComponent as LeftRoundedIcon } from "../assets/icons/left-arrow-rounded.svg";
 export { ReactComponent as AvatarIcon } from "../assets/icons/Avatar.svg";
 export { ReactComponent as AddUserIcon } from "../assets/icons/AddUser.svg";
 export { ReactComponent as TickIcon } from "../assets/icons/tickIcon.svg";
+export { ReactComponent as RightIcon } from "../assets/icons/chevron-right.svg";
+export { ReactComponent as ChatBg } from "../assets/icons/chat-bg.svg";
+export { ReactComponent as MemoryBg } from "../assets/icons/memory-games.svg";
+export { ReactComponent as FaceAssistBg } from "../assets/icons/face-assist.svg";
+export { ReactComponent as VoiceAssistBg } from "../assets/icons/voice-assist.svg";
+export { ReactComponent as ClockIcon } from "../assets/icons/clock.svg";
+export { ReactComponent as PaperClip } from "../assets/icons/paperclip.svg";
+export { ReactComponent as StarIcon } from "../assets/icons/star.svg";
+export { ReactComponent as MemoryIcon } from "../assets/icons/memory.svg";
+export { ReactComponent as PieIcon } from "../assets/icons/pie-chart.svg";
+export { ReactComponent as HomeIcon } from "../assets/icons/home.svg";
+
+
+
+
+
+
